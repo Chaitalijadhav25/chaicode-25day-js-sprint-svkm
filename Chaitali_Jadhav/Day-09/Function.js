696641c93b39b7790ff788468d2bbc08de4d8957
Chaitalijadhav25 <chaitalijadhav250107@gmail.com>
2026-06-10 18:20:42 +0530

    Day 9 - added function - Chaitali jadhav

--- Chaitali_Jadhav/Day-09/Function.js
+++ Chaitali_Jadhav/Day-09/Function.js
@@ -75,7 +75,7 @@ function returnSecondValue(getArray){
 }
 
 console.log(returnSecondValue(myNewArray));
-// console.log(returnSecondValue([200, 400, 300 , 400]));
+console.log(returnSecondValue([200, 400, 300 , 400]));
 
 
 
